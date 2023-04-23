ba71b8fff0f727bbfa726d2bd574bf56c2b5b4cb
Kelvin Santos <felipedossantoskelvin@gmail.com>
2023-04-23 19:50:39 -0300

    Update automocao.py

--- automocao.py
+++ automocao.py
@@ -2,55 +2,63 @@ import speech_recognition as sr
 import os
 import pyttsx3
 
-r = sr.Recognizer()
+class Action :
+    def __init__(self):
+        self.r = sr.Recognizer()
+        self.engine = pyttsx3.init()
+        self.action  = {
+            '1': r'C:/Users/Kelvin/Desktop/jogos/Resident Evil 2.url',
+            '2': r'C:/Users/Kelvin/Desktop/jogos/CsGo.url',
+            '3': r'C:/Users/Kelvin/Desktop/jogos/VALORANT.lnk'
+        }
 
-engine = pyttsx3.init()
-
-with sr.Microphone() as source: 
-    engine.say("Olá, qual jogo você gostaria de jogar?")
-    engine.say("1- resident evil, 2- counter strinke, 3- valorant ")
-    engine.runAndWait()
+    def run(self):
+        with sr.Microphone() as source: 
+            self.engine.say("Olá, qual jogo você gostaria de jogar?")
+            self.engine.say("1- resident evil, 2- counter strinke, 3- valorant")
+            self.engine.say("Ou se quiser sair do programa fale fechar")
+            self.engine.runAndWait()
+            
+            while True:
+                audio = self.r.listen(source)
+                try:
+                    text = self.r.recognize_google(audio, language='pt-BR')
+                    print("Você disse: {}".format(text))
+                    if '1' in text.lower():
+                        os.startfile(self.action['1'])
+                        self.engine.say('Jogo aberto, divirta-se!')
+                        self.engine.runAndWait()
+                        
+                    elif '2' in text.lower():
+                        os.startfile(self.action['2'])
+                        print("Jogo aberto!")
+                        self.engine.say("Jogo aberto!")
+                        self.engine.runAndWait()
     
-    while True:
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio, language='pt-BR')
-            print("Você disse: {}".format(text))
-            if '1' in text.lower():
-                jogo_path2 = r""
-                os.startfile(jogo_path2)
-                engine.say('Jogo aberto, divirta-se!')
-                engine.runAndWait()
+                    elif '3' in text.lower():
+                        os.startfile(self.action['3'])
+                        self.engine.say('Jogo aberto, divirta-se!')
+                        self.engine.runAndWait()
+    
+                    elif 'fechar' in text.lower():
+                        print("Encerrando o programa...")
+                        self.engine.say("Encerrando o programa...")
+                        self.engine.runAndWait()
+                        break
+    
                 
-            elif '2' in text.lower():
-                jogo_path = r""
-                os.startfile(jogo_path)
-                print("Jogo aberto!")
-                engine.say("Jogo aberto!")
-                engine.runAndWait()
+                    self.engine.say("Algo mais ?")
+                    self.engine.runAndWait()
+    
+                except sr.UnknownValueError:
+                    print("Não foi possível entender o áudio")
+                    self.engine.say("Não foi possível entender o áudio, por favor repita novamente")
+                    self.engine.runAndWait()
+    
+                except sr.RequestError as e:
+                    print("Erro ao solicitar reconhecimento de fala: {}".format(e))
+                    self.engine.say("Erro ao solicitar reconhecimento de fala")
+                    self.engine.runAndWait()
 
-            elif '3' in text.lower():
-                jogo_path3 = r""
-                os.startfile(jogo_path3)
-                engine.say('Jogo aberto, divirta-se!')
-                engine.runAndWait()
-
-            elif 'fechar' in text.lower():
-                print("Encerrando o programa...")
-                engine.say("Encerrando o programa...")
-                engine.runAndWait()
-                break
-
-        
-            engine.say("Algo mais ?")
-            engine.runAndWait()
-
-        except sr.UnknownValueError:
-            print("Não foi possível entender o áudio")
-            engine.say("Não foi possível entender o áudio, por favor repita novamente")
-            engine.runAndWait()
-
-        except sr.RequestError as e:
-            print("Erro ao solicitar reconhecimento de fala: {}".format(e))
-            engine.say("Erro ao solicitar reconhecimento de fala")
-            engine.runAndWait()
+game_launcher = Action()
+game_launcher.run()
